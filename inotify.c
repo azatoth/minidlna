ff80c0ea243a03aa094765abf618dbf30156f46e
Justin Maggard <jmaggard@users.sourceforge.net>
2009-11-19 18:34:42 +0000

    * Missed one place for the recent db_path change. (Thanks stedy6!)

--- inotify.c
+++ inotify.c
@@ -543,7 +543,7 @@ inotify_remove_file(const char * path)
 		sql_exec(db, "DELETE from DETAILS where ID = %lld", detailID);
 		sql_exec(db, "DELETE from OBJECTS where DETAIL_ID = %lld", detailID);
 	}
-	asprintf(&art_cache, "%s/art_cache%s", DB_PATH, path);
+	asprintf(&art_cache, "%s/art_cache%s", db_path, path);
 	remove(art_cache);
 	free(art_cache);
 
