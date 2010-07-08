2005d10ab310d357a7000cf579f83bcb1f060255
Justin Maggard <jmaggard@users.sourceforge.net>
2010-07-08 00:29:37 +0000

    Add a new Samsung User-Agent for client type detection.

--- upnphttp.c
+++ upnphttp.c
@@ -247,7 +247,8 @@ intervening space) by either an integer or the keyword "infinite". */
 					h->reqflags |= FLAG_DLNA;
 					h->reqflags |= FLAG_MIME_AVI_DIVX;
 				}
-				else if(strncmp(p, "SamsungWiselinkPro", 18)==0)
+				else if(strncmp(p, "SamsungWiselinkPro", 18)==0 ||
+				        strncmp(p, "SEC_HHP_TV", 10)==0)
 				{
 					h->req_client = ESamsungTV;
 					h->reqflags |= FLAG_DLNA;
