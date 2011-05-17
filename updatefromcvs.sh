c33cec25a241d8284406885f33da3e0ccaafe2dc
Carl Fürstenberg <azatoth@gmail.com>
2011-05-17 23:09:32 +0200

    Add Justin to temporary authors files

--- updatefromcvs.sh
+++ updatefromcvs.sh
@@ -7,7 +7,12 @@ if [ "$(git status --porcelain --untracked-files=no)" != "" ]; then
 	exit 1;
 fi
 branch=$(git name-rev --name-only HEAD)
+authors=$(tempfile)
+cat << EOF > $authors
+jmaggard=Justin Maggard <jmaggard@users.sourceforce.net>
+EOF
 
 git checkout cvs
-git cvsimport -d:pserver:anonymous@minidlna.cvs.sourceforge.net:/cvsroot/minidlna -o cvs -k -u -m minidlna
+git cvsimport -d:pserver:anonymous@minidlna.cvs.sourceforge.net:/cvsroot/minidlna -o cvs -k -u -m -A $authors minidlna
 git checkout $branch
+unlink $authors
